63a775cad1c12607a74db22db2256235a17e891f
Pape Cheikh Mbaye <papecheikhmbaye@esp.sn>
2026-04-24 17:55:16 +0000

    Lab 2 : composants statiques LigneBus et ListeLignes

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,19 @@
 import "./Header.css";
+
 function Header() {
+  const date = new Date().toLocaleDateString("fr-FR");
+
   return (
     <header className="header">
       <h1 className="header-titre">SenTransport</h1>
+
       <p className="header-soustitre">
         Votre guide du transport en commun a Dakar
       </p>
+
+      <p>{date}</p>
     </header>
   );
 }
+
 export default Header;
